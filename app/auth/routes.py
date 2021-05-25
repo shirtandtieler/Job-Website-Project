cd00b305d5f32294c0f4070ed03db2536743a75b
Tyler Wolfe-Adam <shirtandtieler@gmail.com>
2021-05-25 13:00:50 -0500

    Fix account creation
    To use API funcs

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,8 @@
 from flask import url_for, flash, render_template
 from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.utils import redirect
+
+from app.api.users import new_seeker, new_company
 from app.auth import bp
 from app import db
 from app.auth.forms import LoginForm, RegisterForm
@@ -42,14 +44,17 @@ def register():
             flash(f'User {form.email.data} is already registered!')
             return redirect(url_for('auth.login'))
         # create new user and add to the database
-        # TODO Bad practice to interface w/db directly. Replace with API
         atype = AccountTypes.s if form.account_type.data.lower().startswith("s") else AccountTypes.c
-        user = User(account_type = atype, email = form.email.data)
-        user.set_password(form.password.data)  # hashes it
-        db.session.add(user)
-
-        ## TODO Add external trigger to create a seeker or company profile when user accounts are added (or change table structure)
-
-        db.session.commit()
+        if atype == 's':
+            new_func = new_seeker
+        else:
+            new_func = new_company
+        new_func(form.email.data, form.password.data)
+        # atype = AccountTypes.s if form.account_type.data.lower().startswith("s") else AccountTypes.c
+        # user = User(account_type = atype, email = form.email.data)
+        # user.set_password(form.password.data)  # hashes it
+        # db.session.add(user)
+        #
+        # db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('register.html', title='Register', form=form)
